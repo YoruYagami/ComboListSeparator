46d2042b19d9e0650dfa53eb3ee1add1e2210bb9
YoruYagami <70035442+YoruYagami@users.noreply.github.com>
2023-02-22 13:01:43 +0100

    Update separate.sh

--- separate.sh
+++ separate.sh
@@ -13,8 +13,8 @@ if [[ ! -f $1 ]]; then
 fi
 
 # Create output files
-username_file="username.txt"
-password_file="password.txt"
+username_file=$(dirname "$1")/username.txt
+password_file=$(dirname "$1")/password.txt
 > "$username_file"
 > "$password_file"
 
@@ -27,3 +27,8 @@ while read -r line; do
   echo "$username" >> "$username_file"
   echo "$password" >> "$password_file"
 done < "$1"
+
+# Print success message and file paths
+echo -e "\033[32mThe separation was successful\033[0m"
+echo "Username file: $username_file"
+echo "Password file: $password_file"
